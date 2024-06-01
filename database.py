88422bc9529d9e4ce1d7c64c4c07339b3e251063
George <110062762+georgebarr@users.noreply.github.com>
2024-06-01 17:06:35 +0100

    Added option to update a contact's data, also fixed some formatting

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ def create_contact(contact):
     Inserts a contact into the database
 
     Args:
-    contact (Contact): An instance of the contact class  
+    	contact (Contact): An instance of the Contact class  
     """
 
 	with con: 
@@ -28,7 +28,7 @@ def view_contact_information(contact):
     Returns the information of a specific contact within the database
 
     Args:
-    contact (Contact): An instance of the contact class  
+    	contact (Contact): An instance of the Contact class  
     """
 
 	cur.execute("SELECT * FROM contacts WHERE surname=:surname", {'surname': contact.surname})
@@ -37,13 +37,40 @@ def view_contact_information(contact):
 	return cur.fetchall()
 
 
+def update_data(old_value, new_value, option):
+
+	"""
+	Allows the user to update a contact's phone or email.
+
+	Args:
+		old_value (str): The contact's current information which they would like to change
+		new_value (str): What the contact's new information should be
+		option (str): Either phone or email, determines what is updated.
+	"""
+
+	data = {
+		"new_value": new_value,
+		"old_value": old_value
+		}
+
+	if option == "phone":
+
+		cur.execute("UPDATE contacts SET phone = :new_value WHERE phone = :old_value", 
+			data)
+
+	else:
+
+		cur.execute("UPDATE contacts SET email = :new_value WHERE email = :old_value", 
+			data)
+
+
 def remove_contact(contact):
 
 	"""
     Removes a contact from the database
 
     Args:
-	contact (Contact): An instance of the contact class  
+		contact (Contact): An instance of the Contact class  
    	"""
 
 	with con: 
